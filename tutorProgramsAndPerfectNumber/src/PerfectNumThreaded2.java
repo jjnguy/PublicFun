8327b88c82df11a48de4e1e53f5bf1155e779291
jjnguy <justin.john.nelson@gmail.com>
2008-09-29 05:14:36 +0000

    fixed with any thing other than single or dual core

--- tutorProgramsAndPerfectNumber/src/PerfectNumThreaded2.java
+++ tutorProgramsAndPerfectNumber/src/PerfectNumThreaded2.java
@@ -5,15 +5,15 @@ public class PerfectNumThreaded2 {
 	public static int MAX_THREADS = Runtime.getRuntime().availableProcessors();
 	public static List<Integer> nums = new ArrayList<Integer>();
 	public static int found = 0;
-
+	
 	public static void main(String[] args) throws Exception {
 		for (int i = 0; i < MAX_THREADS; i++) {
-			PerfectNumThread2 th1 = new PerfectNumThread2(6 + MAX_THREADS * i);
+			PerfectNumThread2 th1 = new PerfectNumThread2(6 + 2 * i);
 			th1.start();
 		}
 		System.out.println("Num threads: " + MAX_THREADS);
 	}
-
+	
 	public static boolean isPerfect(int num) {
 		int factorSum = 0;
 		int halfnum = num / 2 + 1;
@@ -32,13 +32,13 @@ public class PerfectNumThreaded2 {
 }
 
 class PerfectNumThread2 extends Thread {
-
+	
 	int startNum;
-
+	
 	public PerfectNumThread2(int num) {
 		this.startNum = num;
 	}
-
+	
 	@Override
 	public void run() {
 		long startTime = System.currentTimeMillis();
@@ -50,7 +50,10 @@ class PerfectNumThread2 extends Thread {
 				System.out.println(num);
 			}
 			num += PerfectNumThreaded2.MAX_THREADS * 2;
+			if (num % 100000 == 0) {
+				System.out.println("Checking: " + num);
+			}
 		}
-		System.out.println("Total time: " + (System.currentTimeMillis() - startTime)/1000 + " seconds.");
+		System.out.println("Total time: " + (System.currentTimeMillis() - startTime) / 1000 + " seconds.");
 	}
 }
