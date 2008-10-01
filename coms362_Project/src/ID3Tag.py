8abfcd8799ec543fa0c318a55cedabb8c6078ac9
jjnguy <justin.john.nelson@gmail.com>
2008-10-01 07:40:12 +0000

    working on tag reading, v2 TXX tags work now

--- coms362_Project/src/ID3Tag.py
+++ coms362_Project/src/ID3Tag.py
@@ -10,23 +10,27 @@ class ID3v2_3Tag:
         self.frames = []
         bytes_left = self.headder.len
         
-        
-        #while(bytes_left):
-        if (self.headder.major_version < 3):
-            while bytes_left:
+        #legacy support
+        if self.headder.major_version < 3:
+            while bytes_left > 0:
                 fr_head = ID3v2_2FrameHeader(self.file.read(6))
+                fr_head.pritnIt()
                 bytes_left-=6
-                frame = ID3v2_2Frame
+                frame = ID3v2_2Frame(fr_head, self.file.read(fr_head.frame_len))
+                frame.printIt()
+                bytes_left-=fr_head.frame_len
+                print "Bytes left in the tag: %i" % bytes_left
         else:
             fr_head = ID3v2_3FrameHeader(self.file.read(10))
-        fr_head.pritnIt()
         
 
 class ID3v2_3TagHeader:
     def __init__(self, ten_byte_string):
         self.whole_string = ten_byte_string
-        self.file_identifier, self.major_version, self.minor_version, self.flags, self.len \
- = struct.unpack('>3sBBBI', self.whole_string)
+        self.file_identifier, self.major_version, self.minor_version, self.flags, b1, b2, b3, b4 = struct.unpack('>3sBBBBBBB', self.whole_string)
+        #the tag size encoding is lame!
+        self.len = (b1 << 21) + (b2 << 14) + (b3 << 7) + b4
+        
         if (self.file_identifier != "ID3"):
             print "FUCK!"
         if (not self.noFlags()):
@@ -36,10 +40,8 @@ class ID3v2_3TagHeader:
     
     def noFlags(self):
         if (self.flags != 0):
-            print "reading this may fail"
             return False
         return True
-        
         
     def printIt(self):
         print "ID: %s" % self.file_identifier
@@ -48,13 +50,23 @@ class ID3v2_3TagHeader:
         print "Len: %s" % self.len
 
 class ID3v2_2Frame:
-    def __init__(self):
-        print "stuf v2"
+    def __init__(self, header, byte_string):
+        if header.frame_id[0] == "T":
+            self.data = struct.unpack("%is" % header.frame_len, byte_string)[0].strip()
+        else:
+            self.data = "Complex Shit"
+        
+    def frameName(self):
+        return header.frame_id
+    
+    def printIt(self):
+        print self.data
 
 class ID3v2_2FrameHeader:
-    def __init__(self, ten_byte_string):
-        self.frame_id, b1, b2, b3 = struct.unpack(">3sBBB", ten_byte_string)
-        self.frame_len = (b1 << 8) + (b2 << 4) + b3
+    def __init__(self, six_byte_string):
+        self.frame_id, b1, b2, b3 = struct.unpack(">3sBBB", six_byte_string)
+        #building ints from 3 bytes is lame
+        self.frame_len = (b1 << 16) + (b2 << 8) + b3
         
     def pritnIt(self):
         print self.frame_id
@@ -74,6 +86,5 @@ class ID3v2_3FrameHeader:
         print self.frame_len
         print self.status_mes
         print self.format_desc
-        
-
-tag = ID3v2_3Tag("12 Sex Rap.mp3")
+       
+tag = ID3v2_3Tag("10_Pet.mp3")
