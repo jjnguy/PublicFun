bb7480fba70a523eced36056bfb7ae6f86eb37e3
jjnguy <justin.john.nelson@gmail.com>
2008-10-01 17:05:34 +0000

    fixed padding, probably

--- coms362_Project/src/ID3Tag.py
+++ coms362_Project/src/ID3Tag.py
@@ -4,7 +4,7 @@ class ID3v2_3Tag:
     def __init__(self, mp3_filename):
         self.file = open(mp3_filename, "rb")
         # create the header using the first ten bytes in the file
-        self.header = ID3v2_3TagHeader(self.file.read(10))
+        self.header = ID3v2_XTagHeader(self.file.read(10))
         self.header.printIt()
         self.header.noFlags()
         self.frames = []
@@ -16,28 +16,40 @@ class ID3v2_3Tag:
                 fr_head = ID3v2_2FrameHeader(self.file.read(6))
                 fr_head.pritnIt()
                 bytes_left-=6
+                if fr_head.frame_len == 0:
+                    self.file.read(bytes_left)
+                    bytes_left = 0;
+                    continue
                 frame = ID3v2_2Frame(fr_head, self.file.read(fr_head.frame_len))
                 frame.printIt()
                 bytes_left-=fr_head.frame_len
                 print "Bytes left in the tag: %i" % bytes_left
         else:
-            print "take up space"
-            # TODO support the current standard
+            while bytes_left > 0:
+                fr_head = ID3v2_3FrameHeader(self.file.read(10))
+                fr_head.pritnIt()
+                bytes_left-=10
+                frame = ID3v2_3Frame(fr_head, self.file.read(fr_head.frame_len))
+                frame.printIt()
+                bytes_left-=fr_head.frame_len
+                print "Bytes left in the tag: %i" % bytes_left
         
 
-class ID3v2_3TagHeader:
+class ID3v2_XTagHeader:
     def __init__(self, ten_byte_string):
         self.whole_string = ten_byte_string
         self.file_identifier, self.major_version, self.minor_version, self.flags, b1, b2, b3, b4 = struct.unpack('>3sBBBBBBB', self.whole_string)
-        #the tag size encoding is lame!
+        
+        #the tag size encoding is lame! 28 bits, how stupid
+        #whats wrong with 32? Too big my ass!
         self.len = (b1 << 21) + (b2 << 14) + (b3 << 7) + b4
         
         if (self.file_identifier != "ID3"):
-            print "FUCK!"
+            print "This is not an ID3 tag"
         if (not self.noFlags()):
             print "I will probably fail soon"
         if(self.major_version < 3):
-            print "great, now we are in compatibility mode"
+            print "Parsing v2.2 tag"
     
     def noFlags(self):
         if (self.flags != 0):
@@ -66,7 +78,7 @@ class ID3v2_2Frame:
         return header.frame_id
     
     def printIt(self):
-        print self.data
+        print "Data: %s" % self.data
 
 class ID3v2_2FrameHeader:
     def __init__(self, six_byte_string):
@@ -75,8 +87,8 @@ class ID3v2_2FrameHeader:
         self.frame_len = (b1 << 16) + (b2 << 8) + b3
         
     def pritnIt(self):
-        print self.frame_id
-        print self.frame_len
+        print "Frame ID: %s" % self.frame_id
+        print "Frame Length: %s" % self.frame_len
         
         
 class ID3v2_3Frame:
@@ -93,4 +105,5 @@ class ID3v2_3FrameHeader:
         print self.status_mes
         print self.format_desc
        
-tag = ID3v2_3Tag("12 Sex Rap.mp3")
+#TODO make sure that padding fix actually works
+tag = ID3v2_3Tag("03 Sonny.mp3")
