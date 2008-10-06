904e84d122f348a78333644829bf1875a9599434
jjnguy <justin.john.nelson@gmail.com>
2008-10-06 04:31:32 +0000

    commit before huge refactor to int instead of byte

--- coms_362_java/src/id3TagStuff/ID3v2_2Frame.java
+++ coms_362_java/src/id3TagStuff/ID3v2_2Frame.java
@@ -8,20 +8,19 @@ import java.io.IOException;
 import java.io.InputStream;
 
 public class ID3v2_2Frame implements ID3v2_XFrame {
+
 	private ID3v2_2FrameHeader header;
-	private String tagID;
 	private ID3v2_2FrameData data;
 
 	public ID3v2_2Frame(byte[] headerBytes, InputStream tagFile) throws IOException {
 		header = new ID3v2_2FrameHeader(headerBytes);
 		byte[] frameBytes = new byte[header.getSize()];
 		tagFile.read(frameBytes);
-		tagID = header.getID();
-		if (tagID.startsWith("T", 0)) {
+		if (header.getID().startsWith("T", 0)) {
 			data = new ID3_String(frameBytes);
-		} else if (tagID.equals("COM")) {
+		} else if (header.getID().equals("COM")) {
 			data = new ID3_Comment(frameBytes);
-		} else if (tagID.equals("PIC")) {
+		} else if (header.getID().equals("PIC")) {
 			data = new ID3_Picture(frameBytes);
 		}
 	}
--- coms_362_java/src/id3TagStuff/id3Data/ID3_Picture.java
+++ coms_362_java/src/id3TagStuff/id3Data/ID3_Picture.java
@@ -17,13 +17,20 @@ public class ID3_Picture implements ID3v2_2FrameData {
 	private byte type;
 	private byte[] data;
 
-	public ID3_Picture(byte[] data) throws IOException {
-		format = new String(Arrays.copyOfRange(data, 0, 3));
-		type = data[3];
-		description = new String(Arrays.copyOfRange(data, 4, data.length))	
-				.split(new String(new byte[] { 0 }))[0];
-		data = Arrays.copyOfRange(data, description.length(), data.length);
-		for (int i = 0; i < data.length; i++) {
+	public ID3_Picture(byte[] dataP) throws IOException {
+		format = new String(Arrays.copyOfRange(dataP, 0, 3));
+		type = dataP[3];
+		int descWidth = 0;
+		for (int i = 4; i < dataP.length; i++) {
+			if (dataP[i] == (byte) 00) {
+				break;
+			}
+			descWidth++;
+		}
+		description = new String(Arrays.copyOfRange(dataP, 4, 4 + descWidth));
+		this.data = Arrays.copyOfRange(dataP, 4 + descWidth, dataP.length);
+
+		for (int i = 0; i < this.data.length; i++) {
 			if (data[i] < 0) {
 				data[i] = (byte) (data[i] & 0xff);
 			}
@@ -31,7 +38,7 @@ public class ID3_Picture implements ID3v2_2FrameData {
 		// begin test code
 		PrintStream out = new PrintStream(new File(".dataTest"));
 		out.write(data);
-		//BufferedImage i = ImageIO.read(new File(".dataTest"));
-		//ImageIO.write(i, format, new File("imageTest.png"));
+		BufferedImage i = ImageIO.read(new File(".dataTest"));
+		ImageIO.write(i, format, new File("imageTest.png"));
 	}
 }
