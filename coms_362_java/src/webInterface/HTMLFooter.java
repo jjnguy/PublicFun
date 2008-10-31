ff31f4db9dd5828c835e3b34dae8ce34478a13bf
jjnguy <justin.john.nelson@gmail.com>
2008-10-31 17:43:56 +0000

    justin

--- coms_362_java/src/webInterface/HTMLFooter.java
+++ coms_362_java/src/webInterface/HTMLFooter.java
@@ -10,6 +10,7 @@ public class HTMLFooter {
 		StringBuilder builder = new StringBuilder();
 		builder.append("<div class=\"footer center whiteOnBlack\">\n");
 		builder.append("\t<a href=\"http://www.google.com\"  class=\"button plainText\">Click to search</a>\n");
+		// TODO this should use the author names
 		builder.append("\tby Justin Nelson, Ben Petersen, and Shaun SomeLastname (sorry Shaun)\n");
 		builder.append("</div>");
 		code = builder.toString();
