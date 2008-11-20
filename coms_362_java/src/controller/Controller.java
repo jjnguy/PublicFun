27fd8a6b3d101d800a5eec5662b58902c5e1f697
jjnguy <justin.john.nelson@gmail.com>
2008-11-20 05:09:32 +0000

    jjn, got a preliminary controller set up

---
+++ coms_362_java/src/controller/Controller.java
@@ -0,0 +1,16 @@
+package controller;
+
+import java.io.InputStream;
+
+import webViewInterface.UploadSong;
+
+public class Controller implements UploadSong {
+
+	@Override
+	public String uploadSong(InputStream fileStream) {
+		// TODO Needs to call the appropriate classes to store
+		// the song into a DB and save the file in the correct location
+		return null;
+	}
+	
+}
