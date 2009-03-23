2959756312c2be0052f8faa8117cc197b3ea3d6e
jjnguy <justin.john.nelson@gmail.com>
2009-03-23 22:24:32 +0000

    stuff

--- gravityProject/src/GravityFrame.java
+++ gravityProject/src/GravityFrame.java
@@ -1,6 +1,9 @@
 import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
+import java.awt.event.WindowStateListener;
 import java.io.FileNotFoundException;
 import java.util.Timer;
 import java.util.TimerTask;
@@ -18,7 +21,7 @@ public class GravityFrame extends JFrame {
 	private Timer t;
 
 	public GravityFrame() throws FileNotFoundException {
-		super();
+		super("Gravity Fun!");
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		gravPane = new GravityPane();
 		advanceButton = new JButton("Start");
@@ -31,12 +34,12 @@ public class GravityFrame extends JFrame {
 		pack();
 		int hiehgt = gravPane.getHeight();
 		gravPane.addObject(new GravitySphere(200, hiehgt - 30));
-		gravPane.addObject(new GravitySphere(80, 0, 120, 0, .9, 100,
-				Color.GREEN));
 		gravPane
 				.addObject(new GravitySphere(90, 0, 120, 0, .5, 60, Color.BLUE));
 		gravPane.addObject(new GravitySphere(100, 0, 120, 0));
 		gravPane.addObject(new GravitySphere(300, hiehgt + 1000));
+		gravPane
+				.addObject(new GravitySphere(80, 0, 120, 0, .9, 20, Color.GREEN));
 		running = false;
 		t = new Timer();
 	}
