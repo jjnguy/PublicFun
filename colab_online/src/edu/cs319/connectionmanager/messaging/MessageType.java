66860db1bfa31d0317f7bf9d635939a60ab7f0a0
Wayne Radcliff <justin.john.nelson@gmail.com>
2009-04-08 02:32:52 +0000

    Changed the stuff...

--- colab_online/src/edu/cs319/connectionmanager/messaging/MessageType.java
+++ colab_online/src/edu/cs319/connectionmanager/messaging/MessageType.java
@@ -1,14 +1,21 @@
 package edu.cs319.connectionmanager.messaging;
 
 public enum MessageType {
-	TEXT_ADDED((byte) 0x00, "Text Added"), TEXT_CHANGED((byte) 0x01, "Text Changed"), TEXT_DELETED(
-			(byte) 0x02, "Text Deleted"), TEXT_HIGHLIGHTED((byte) 0x03, "Text Highlighted"), TEXT_UNHIGHLIGHTED(
-			(byte) 0x04, "Text UnHighlighted"), NEW_MESSAGE((byte) 0x05, "New Message"), NEW_PRIVATE_MESSAGE(
-			(byte) 0x06, "New Private Message"), NEW_CLIENT((byte) 0x07, "New Client"), MEMBER_JOIN_ROOM(
-			(byte) 0x08, "Member Joined Room"), MEMBER_LEAVE_ROOM((byte) 0x09, "Member Left Room"), CHANGE_USER_PRIV(
-			(byte) 0x0a, "Changed User Priv"), GET_ROOM_LIST((byte) 0x0b, "Get Room List"), NEW_COLAB_ROOM(
-			(byte) 0x0c, "New CoLab Room"), COMMUNICATION_FAIL((byte) 0x0d,
-			"A previous communication attempt failed");
+	UPDATE_ALL_SUBSECTIONS((byte) 0x00, "Update All Subsections"), 
+	UPDATE_SUBSECTION((byte) 0x01, "Update Subsection"), 
+	SUBSECTION_LOCKED((byte) 0x02, "Subsection Locked"), 
+	SUBSECTION_UNLOCKED((byte) 0x03, "Subsection Unlocked"), 
+	NEW_SUBSECTION((byte) 0x04, "New Subsection"),
+	NEW_MESSAGE((byte) 0x05, "New Message"), 
+	NEW_PRIVATE_MESSAGE((byte) 0x06, "New Private Message"), 
+	NEW_CLIENT((byte) 0x07, "New Client"), 
+	MEMBER_JOIN_ROOM((byte) 0x08, "Member Joined Room"), 
+	MEMBER_LEAVE_ROOM((byte) 0x09, "Member Left Room"), 
+	MEMBERS_IN_ROOM((byte) 0x0a, "Members In Room"),
+	CHANGE_USER_PRIV((byte) 0x0b, "Changed User Priv"), 
+	GET_ROOM_LIST((byte) 0x0c, "Get Room List"), 
+	NEW_COLAB_ROOM(	(byte) 0x0d, "New CoLab Room"), 
+	COMMUNICATION_FAIL((byte) 0x0e,	"A previous communication attempt failed");
 
 	private byte code;
 	private String name;
@@ -30,15 +37,15 @@ public enum MessageType {
 	public static MessageType getMessageTypeByCode(byte code) {
 		switch (code) {
 		case 0x0:
-			return TEXT_ADDED;
+			return UPDATE_ALL_SUBSECTIONS;
 		case 0x1:
-			return TEXT_CHANGED;
+			return UPDATE_SUBSECTION;
 		case 0x2:
-			return TEXT_DELETED;
+			return SUBSECTION_LOCKED;
 		case 0x3:
-			return TEXT_HIGHLIGHTED;
+			return SUBSECTION_UNLOCKED;
 		case 0x4:
-			return TEXT_UNHIGHLIGHTED;
+			return NEW_SUBSECTION;
 		case 0x5:
 			return NEW_MESSAGE;
 		case 0x6:
@@ -50,12 +57,14 @@ public enum MessageType {
 		case 0x9:
 			return MEMBER_LEAVE_ROOM;
 		case 0xa:
-			return CHANGE_USER_PRIV;
+			return MEMBERS_IN_ROOM;
 		case 0xb:
-			return GET_ROOM_LIST;
+			return CHANGE_USER_PRIV;
 		case 0xc:
-			return NEW_COLAB_ROOM;
+			return GET_ROOM_LIST;
 		case 0xd:
+			return NEW_COLAB_ROOM;
+		case 0xe:
 			return COMMUNICATION_FAIL;
 		default:
 			return null;
