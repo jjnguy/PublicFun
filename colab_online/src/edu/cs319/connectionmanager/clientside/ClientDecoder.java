8b9f85ba5441f771e5b629cacab5d330e6adc1e0
jjnguy <justin.john.nelson@gmail.com>
2009-04-08 04:21:37 +0000

    encoding and decoding done

--- colab_online/src/edu/cs319/connectionmanager/clientside/ClientDecoder.java
+++ colab_online/src/edu/cs319/connectionmanager/clientside/ClientDecoder.java
@@ -2,10 +2,14 @@ package edu.cs319.connectionmanager.clientside;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 import edu.cs319.client.IClient;
 import edu.cs319.connectionmanager.messaging.Message;
 import edu.cs319.connectionmanager.messaging.MessageInputStream;
+import edu.cs319.dataobjects.DocumentSubSection;
+import edu.cs319.server.CoLabPrivilegeLevel;
 import edu.cs319.util.NotYetImplementedException;
 import edu.cs319.util.Util;
 
@@ -44,22 +48,52 @@ public class ClientDecoder implements Runnable {
 		}
 	}
 
+	/*
+	 * COMMUNICATION_FAIL((byte) 0x0e, REMOVE_SUBSECTION((byte) 0x0f,
+	 */
+
 	private void decodeMessage(Message message) throws IOException {
+		String username = message.getSentByClientName();
+		List<String> arg = message.getArgumentList();
 		switch (message.getMessageType()) {
 		case NEW_MESSAGE:
-			actualClient.newChatMessage(message.getSentByClientName(), message.getArgumentList()
-					.get(0));
+			actualClient.newChatMessage(username, arg.get(0));
 			break;
 		case NEW_PRIVATE_MESSAGE:
-			actualClient.newChatMessage(message.getSentByClientName(), message.getArgumentList()
-					.get(0), message.getArgumentList().get(1));
+			actualClient.newChatMessage(username, arg.get(0), arg.get(1));
 			break;
 		case MEMBER_JOIN_ROOM:
-			actualClient.coLabRoomMemberArrived(message.getSentByClientName());
+			actualClient.coLabRoomMemberArrived(username);
 			break;
+		case UPDATE_ALL_SUBSECTIONS:
+			List<DocumentSubSection> args = new ArrayList<DocumentSubSection>();
+			for (int i = 1; i < arg.size(); i++) {
+				args.add(DocumentSubSection.getFromDelimmitedString(arg.get(i)));
+			}
+			actualClient.updateAllSubsections(arg.get(0), args);
+		case UPDATE_SUBSECTION:
+			actualClient.updateSubsection(username, arg.get(0), DocumentSubSection
+					.getFromDelimmitedString(arg.get(1)), arg.get(2));
+		case SUBSECTION_LOCKED:
+			actualClient.subsectionLocked(username, arg.get(0), arg.get(1));
+		case SUBSECTION_UNLOCKED:
+			actualClient.subsectionUnLocked(username, arg.get(0), arg.get(1));
+		case NEW_SUBSECTION:
+			actualClient.newSubSection(username, arg.get(0), arg.get(1), DocumentSubSection
+					.getFromDelimmitedString(arg.get(2)), Integer.parseInt(arg.get(3)));
+		case MEMBER_LEAVE_ROOM:
+			actualClient.coLabRoomMemberLeft(username);
+		case MEMBERS_IN_ROOM:
+			actualClient.allUsersInRoom(arg);
+		case CHANGE_USER_PRIV:
+			actualClient.changeUserPrivilege(username, CoLabPrivilegeLevel
+					.getPrivilegeLevelFromString(arg.get(0)));
+		case GET_ROOM_LIST:
+			actualClient.allCoLabRooms(arg);
+		case REMOVE_SUBSECTION:
+			actualClient.subSectionRemoved(username, arg.get(0), arg.get(1));
 		default:
 			throw new NotYetImplementedException();
 		}
 	}
-
 }
