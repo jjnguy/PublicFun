95102e994ebb26899d6e4e6b09e97f6861699037
Ian Dallas <justin.john.nelson@gmail.com>
2009-04-26 21:47:05 +0000

    Fixed an error where authenticateUser would throw a NullPointerException if the username did no exist in the database

--- colab_online/src/edu/cs319/database/UsersUtil.java
+++ colab_online/src/edu/cs319/database/UsersUtil.java
@@ -12,73 +12,76 @@ import edu.cs319.dataobjects.database.CS319User;
 public class UsersUtil {
 
 	/**
-	 * Will return true if the username exists and the password
-	 * matches for that user
+	 * Will return true if the username exists and the password matches for that user
+	 * 
 	 * @param user
 	 * @param pass
 	 * @return
 	 */
-	public static boolean authenticateUser(String username, byte[] password){
+	public static boolean authenticateUser(String username, byte[] password) {
 		Session session = HibernateUtil.getSession();
 		Criteria criteria = session.createCriteria(CS319User.class);
-		
+
 		criteria.add(Restrictions.eq("username", username));
-		
+
 		CS319User dbUser = (CS319User) criteria.uniqueResult();
-		
-		boolean passEqual = (Arrays.equals(password,dbUser.getPassword()));
-		System.out.println("VALID PASSWORD = " + passEqual);
-		
-		if(passEqual){
-			return true;
+
+		if (dbUser == null) {
+			return false;
+		}else {
+
+			boolean passEqual = (Arrays.equals(password, dbUser.getPassword()));
+			System.out.println("VALID PASSWORD = " + passEqual);
+
+			if (passEqual) {
+				return true;
+			}
 		}
-		
 		return false;
 	}
-	
+
 	/**
-	 * Will return true if the user has been succesfully created and the username
-	 * did not exist. Will return false if the username already exists
+	 * Will return true if the user has been succesfully created and the username did not exist.
+	 * Will return false if the username already exists
 	 * 
 	 * @param username
 	 * @param password
 	 * @return
 	 */
-	public static boolean createUser(String username, byte[] password){
+	public static boolean createUser(String username, byte[] password) {
 		Session session = HibernateUtil.getSession();
-		
+
 		CS319User newUser = new CS319User();
 		newUser.setUsername(username);
 		newUser.setPassword(password);
-		
-		try{
+
+		try {
 			session.save(newUser);
-		}
-		catch(ConstraintViolationException e){
+		} catch (ConstraintViolationException e) {
 			return false;
 		}
-		
+
 		session.flush();
 		session.close();
-		
-		return true;		
+
+		return true;
 	}
-	
-	public static boolean deleteUser(String username){
+
+	public static boolean deleteUser(String username) {
 		Session session = HibernateUtil.getSession();
 		Criteria criteria = session.createCriteria(CS319User.class);
-		
+
 		criteria.add(Restrictions.eq("username", username));
-		
+
 		CS319User user = (CS319User) criteria.uniqueResult();
-		
-		if(user != null){
+
+		if (user != null) {
 			session.delete(user);
 		}
-		
+
 		session.flush();
 		session.close();
-		
+
 		return true;
 	}
 }
