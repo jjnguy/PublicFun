64c27d10bc1c46f4b1639e192eb2126b632adf31
jjnguy <justin.john.nelson@gmail.com>
2009-11-30 06:16:18 +0000

    added the sln

---
+++ cs430/wcf/PackageTrackingService/PackageTrackingService/OrderService.svc.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace PackageTrackingService
+{
+	public class OrderService : IOrderService
+	{
+		public string GetData(int value)
+		{
+			return string.Format("You entered: {0}", value);
+		}
+
+		public CompositeType GetDataUsingDataContract(CompositeType composite)
+		{
+			if (composite.BoolValue)
+			{
+				composite.StringValue += "Suffix";
+			}
+			return composite;
+		}
+
+		#region IOrderService Members
+
+		public string HelloWho(string name)
+		{
+			return "Hello " + name;
+		}
+
+		public int Square(int i)
+		{
+			return i*i;
+		}
+
+		#endregion
+	}
+}
