aafccd373db0bba0674ec76f70b7ac63a22d2a3b
jjnguy <justin.john.nelson@gmail.com>
2011-12-14 23:32:36 +0000

    Checking out ClassCreation.

--- UnitTestXmlParser/src/Main.java
+++ UnitTestXmlParser/src/Main.java
@@ -1,14 +1,16 @@
+import java.io.FileNotFoundException;
+
 import xmlcomponents.Jocument;
 import xmlcomponents.Jode;
-import xmlcomponents.autoparse.AutoParser;
-import domain.TestResultXmlFile;
+import xmlcomponents.autoparse.ClassCreation;
 import domain.mstest.TestRun;
 
 public class Main {
-   public static void main(String[] args) {
+   public static void main(String[] args) throws FileNotFoundException {
       String fileLocation = "C:/Users/U0117691/workspace2/JNUnitXmlFileParser/resources/testResult.xml";
       String otherFileLocation = "D:/Development/Foldering/TestResults/MergedTestResults.xml";
       Jode j = Jocument.load(fileLocation, "TestRun");
+      ClassCreation.createClasses(j, "C:/Users/U0117691/Desktop/output");
       TestRun tesstRun = j.toObject(TestRun.class);
       //TestResultXmlFile file = new TestResultXmlFile(fileLocation);
       System.out.println("WooHoo");
